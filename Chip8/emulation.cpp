7af35cb898f4508061dc66e3db418ee706c9cd34
NotCringeyUsername555 <21chota@lexingtonma.org>
2019-01-23 19:51:08 -0500

    These are untested

--- Chip8/emulation.cpp
+++ Chip8/emulation.cpp
@@ -25,33 +25,58 @@ public:
 	void loadGame(const char * c) {
 		fs.open("test.ch8");
 	}
+	void push(char c) {
+		key[sp] = c;
+		sp++;
+	}
+	char pop() {
+		char c = key[sp - 1];
+		key[sp - 1] = 0;
+		return c;
+	}
 	void emulateCycle() {
 		opcode = memory[pc] << 8 | memory[pc + 1];
-		if (opcode & 0x0FFF == 0) {
+		switch (opcode >> 12) {
+		case 0:
 			I = opcode & 0x0FFF;
 			pc += 2;
-		}
-		else if (opcode & 0x0FFF == 0) {
-
-		}
-		else if (opcode == 0x00E0) {
-			for (char c : gfx) {
-				c = 0;
-			}
-		}
-		else if (opcode == 0x00EE) {
-
+			break;
+		case 1:
+			pc = opcode & 0X0FFF;
+			break;
+		case 2:
+			push(pc);
+			pc = opcode & 0x0FFF;
+			break;
+		case 3:
+			if (V[opcode >> 8 & 0x0F] != opcode & 0x00FF) pc += 4;
+			else pc += 2;
+			break;
+		case 4:
+			if (V[opcode >> 8 & 0x0F] == opcode & 0x00FF) pc += 4;
+			else pc += 2;
+			break;
+		case 5:
+			if (V[opcode & 0x0F00 >> 8] != V[opcode & 0x00F0 >> 8]) pc += 2;
+			pc += 2;
+			break;
+		case 6:
+			if (V[opcode & 0x0F00 >> 8] != V[opcode & 0x00F0 >> 8]) pc += 2;
+			pc += 2;
+			break;
 		}
 	}
 };
 int main(int argc, char **argv) {
 	myChip myChip8;
-	init();
+	myChip8.emulateCycle();
+	/*init();
 	myChip8.loadGame("test.ch");
 	for (;;) {
 		myChip8.emulateCycle();
 		if (myChip8.drawFlag)
 			drawGraphics();
 	}
+	return 0;*/
 	return 0;
 }
